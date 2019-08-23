877996ed559820928f50a16866f8054b791665e3
AntonyWP <54414715+AntonyWP@users.noreply.github.com>
2019-08-22 19:25:39 -0500

    Add files via upload

---
+++ assn10pg2.php
@@ -0,0 +1,41 @@
+<?php
+echo "<tr><th> Choose a publisher name, click submit and the page will display the titles of the books that publisher has.</th></tr>";
+$host = "courses";
+$user = "z1624182";
+$password = "1992Jul15";
+$db = "henrybooks";
+
+$conn = new PDO("mysql:host=$host;dbname=$db", $user, $password);
+
+//Our select statement. This will retrieve the data that we want.
+$sql = "SELECT * FROM Publisher";
+
+echo '<form action="assn10pg2.php" method="post">';
+echo 'Select Publisher Name ';
+echo '<SELECT name="Publisher">';
+foreach($conn->query($sql) as $row)
+{
+   echo '<option value = "';
+   echo $row ["PublisherCode"];
+   echo '">';
+   echo $row ["PublisherName"];
+   echo '</option>';
+
+}
+echo '</select>';
+echo '<br><input type="submit" name="submit"  value="Submit"><br>';
+echo '</form>';
+
+if ($_SERVER['REQUEST_METHOD'] == 'POST')
+{
+   $pub = $_POST['Publisher'];
+   $sql = "SELECT distinct book.Title FROM  Book book, Publisher publishet WHERE book.PublisherCode = '$pub'";
+   $row = $conn->query($sql);
+   foreach($row as $r)
+   {
+      echo $r ['Title'];
+   }
+}
+include "footer.html";
+?>
+
